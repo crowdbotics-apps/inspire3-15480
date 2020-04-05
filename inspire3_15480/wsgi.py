e38285d3e3835586874ed27fcc2d74ab7eb91b3d
Crowdbotics <team@crowdbotics.com>
2020-04-05 21:04:52 +0000

    Update models

--- inspire3_15480/wsgi.py
+++ inspire3_15480/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'inspire3_15480.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "inspire3_15480.settings")
 
 application = get_wsgi_application()
