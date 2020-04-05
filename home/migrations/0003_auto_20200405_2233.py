53c4a6bba0933ee9d82631b10b05f3382857140e
Crowdbotics <team@crowdbotics.com>
2020-04-05 22:35:20 +0000

    Update models

--- home/migrations/0003_auto_20200405_2233.py
+++ home/migrations/0003_auto_20200405_2233.py
@@ -6,18 +6,18 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('home', '0002_load_initial_data'),
+        ("home", "0002_load_initial_data"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='homepage',
-            name='content',
+            model_name="homepage",
+            name="content",
             field=models.TextField(blank=True, null=True),
         ),
         migrations.AddField(
-            model_name='homepage',
-            name='footer',
+            model_name="homepage",
+            name="footer",
             field=models.EmailField(blank=True, max_length=254, null=True),
         ),
     ]
